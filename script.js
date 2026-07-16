c14802f523389c695c84500875fa4e91211718ba
Deepak Garikapati <deepakgarikapati999@gmail.com>
2026-07-16 21:54:07 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,175 @@
+// TASK 1:Employee Registration----------
+
+const companyName = "stackly" ;
+let employeeName = prompt("enter employee Name :");
+let employeeID = "EMP101";
+
+console.log("company :" + companyName);
+console.log("Employee Name  :" + employeeName );
+console.log("Employee ID :"  + employeeID);
+
+// Task 2: College Admission Scenario-----
+
+const collegeName ="vignan university";
+let studentName =prompt("enter student name");
+let course =prompt("enter course name");
+
+console.log("College Name :" + collegeName);
+console.log("Student Name :" + studentName);
+console.log("Course :" + course);
+
+// Task 3: Food Order Scenario------
+
+
+const restaurantName ="Pista house";
+let customerName =prompt("enter customer name");
+let foodName=prompt("enter food name");
+let quantity =2;
+
+console.log("Restaurant Name:"  + restaurantName);
+console.log("customer Name:"  + customerName);
+console.log("Food Name:"  + foodName);
+console.log("Quantity:"   + quantity);
+
+// ------TASK 4------ Hospital Registration Scenario------
+
+const hospitalName = "Lalitha";
+let patientName = prompt("enter patient name");
+let tokenNum = prompt("enter token number");
+
+console.log("Hospital Name :" + hospitalName);
+console.log("Patient Name :" + patientName);
+console.log("Token Number :"   + tokenNum);
+
+// Task 5: Movie Ticket Booking Scenario
+
+const theatreName = "PVR Cinemas";
+
+let movieName = prompt("Enter Movie Name:");
+let customerName = prompt("Enter Customer Name:");
+let seatNumber = "A12";
+
+console.log("Theatre : " + theatreName);
+console.log("Movie : " + movieName);
+console.log("Customer : " + customerName);
+console.log("Seat Number : " + seatNumber);
+
+// Task 6: Mobile Shop Scenario------
+
+const shopName = "Sri Mobiles";
+
+let customerName = prompt("Enter Customer Name:");
+let mobileModel = prompt("Enter Mobile Model:");
+let price = 25000;
+
+console.log("Shop : " + shopName);
+console.log("Customer : " + customerName);
+console.log("Mobile Model : " + mobileModel);
+console.log("Price : " + price);
+
+// Task 7: Gym Membership Scenario---------
+
+const gymName = "Fitness Zone";
+
+let memberName = prompt("Enter Member Name:");
+let membershipFee = 1200;
+
+console.log("Gym Name : " + gymName);
+console.log("Member Name : " + memberName);
+console.log("Membership Fee : " + membershipFee);
+
+// Task 8: ATM Welcome Screen Scenario--------
+
+const bankName = "State Bank of India";
+let userName = prompt("Enter User Name:");
+
+console.log("Welcome " + userName + "!");
+console.log("Bank Name : " + bankName);
+
+// Task 9: Bus Ticket Scenario---------
+
+const travelsName = "Orange Travels";
+
+let passengerName = prompt("Enter Passenger Name:");
+let seatNumber = "A10";
+
+console.log("Travels : " + travelsName);
+console.log("Passenger : " + passengerName);
+console.log("Seat Number : " + seatNumber);
+
+// Task 10: Online Shopping Scenario------
+
+const websiteName = "Amazon";
+
+let productName = prompt("Enter Product Name:");
+let customerName = prompt("Enter Customer Name:");
+let price = 49999;
+
+console.log("Website : " + websiteName);
+console.log("Product : " + productName);
+console.log("Customer : " + customerName);
+console.log("Price : " + price);
+
+// Task 11:Console Method Tasks--------
+
+ console.log("Registration Successful");
+
+//  ------Task 12--------
+console.error("Payment Falied");
+
+// -----Task 13-------
+
+console.warn("Battery low");
+
+// ------TASK 14-----
+console.log("Loading...");
+console.log("Welcome");
+
+console.clear();
+
+// Task 15:Alert, Confirm & Prompt Tasks-------
+
+alert("Welcome to Amazon");
+
+// ------Task 16------
+
+var result = confirm("Do you want to logout?");
+console.log(result);
+
+// -----Task 17------
+var city = prompt("Enter your city:");
+
+console.log("Your City : " + city);
+
+// ------Task 18-----
+
+var product = "Laptop";
+product = "Mobile";
+var product = "Tablet";
+
+console.log(product);
+
+// -----Task 19-----
+
+let salary = 25000;
+
+salary = 30000;
+
+console.log(salary);
+
+//-----ask 20------
+
+const company = "Google";
+console.log("Company");
+
+
+
+
+
+
+
+
+
+
+
+
